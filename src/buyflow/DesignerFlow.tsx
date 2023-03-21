1dea8200af2c7c098caf541d7121a29712346ff5
Emmanuel <emmanuelalabi563@gmail.com>
2023-03-21 17:49:14 +0000

    refactor to use flow builder

--- src/buyflow/DesignerFlow.tsx
+++ src/buyflow/DesignerFlow.tsx
@@ -1,34 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ProductIds } from '../types'
-import { PRODUCT_IDS_TO_NAMES } from '../consts'
-import { AgeStep, EmailStep, SummaryStep, NameStep } from '../components'
+import FlowBuilder from './FlowBuilder'
 
 interface BuyflowProps {
   productId: ProductIds
 }
 
 const Designerflow: React.FC<BuyflowProps> = (props) => {
-  const [currentStep, setStep] = useState('email')
-  const [collectedData, updateData] = useState({
-    email: '',
-    age: 0,
-    name: '',
-  })
-  const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value })
-    setStep(nextStep)
-  }
   return (
     <>
-      <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
-      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
-        (currentStep === 'age' && <AgeStep cb={getStepCallback('name')} />) ||
-        (currentStep === 'name' && (
-          <NameStep cb={getStepCallback('summary')} />
-        )) ||
-        (currentStep === 'summary' && (
-          <SummaryStep collectedData={collectedData} />
-        ))}
+      <FlowBuilder
+        initialState={{
+          email: '',
+          age: 0,
+          name: '',
+        }}
+        flow={['email', 'age', 'name']}
+        productId={props.productId}
+      />
     </>
   )
 }
--- src/buyflow/DevFlow.tsx
+++ src/buyflow/DevFlow.tsx
@@ -1,32 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ProductIds } from '../types'
-import { PRODUCT_IDS_TO_NAMES } from '../consts'
-import { AgeStep, EmailStep, SummaryStep } from '../components'
+import FlowBuilder from './FlowBuilder'
 
 interface BuyflowProps {
   productId: ProductIds
 }
 
 const Devflow: React.FC<BuyflowProps> = (props) => {
-  const [currentStep, setStep] = useState('email')
-  const [collectedData, updateData] = useState({
-    email: '',
-    age: 0,
-  })
-  const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value })
-    setStep(nextStep)
-  }
   return (
     <>
-      <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
-      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
-        (currentStep === 'age' && (
-          <AgeStep cb={getStepCallback('summary')} />
-        )) ||
-        (currentStep === 'summary' && (
-          <SummaryStep collectedData={collectedData} />
-        ))}
+      <FlowBuilder
+        initialState={{
+          email: '',
+          age: 0,
+        }}
+        flow={['email', 'age']}
+        productId={props.productId}
+      />
     </>
   )
 }
